3b8f5ea2e8f4548a2c21d683b35bd98dacf0e6c0
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-15 18:45:11 +0100

    fix schema error

--- src/schema.js
+++ src/schema.js
@@ -3,7 +3,7 @@ import { buildASTSchema } from 'graphql';
 import { loader } from 'graphql.macro';
 
 // import the grapqhl model
-const model = loader('../model/runtime.graphql');
+const model = loader('./model/runtime.graphql');
 
 // empty validator (i.e. no form validation)
 const validator = () => {};
