b626343d0802936b9eee63f60a82654de088edea
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-15 18:16:05 +0100

    add schema comments

--- src/schema.js
+++ src/schema.js
@@ -1,9 +1,11 @@
 import { GraphQLBridge } from 'uniforms-bridge-graphql';
-import { buildASTSchema, parse } from 'graphql';
+import { buildASTSchema } from 'graphql';
 import { loader } from 'graphql.macro';
 
+// import the grapqhl model
 const model = loader('../model/runtime.graphql');
 
+// empty validator (i.e. no form validation)
 const validator = () => {};
 
 export const schema = new GraphQLBridge(
