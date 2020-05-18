32f1bedb44e9415510f62cb261e30018c8e45131
Gianluca <gianlucazuccarelli@gmail.com>
2020-05-18 15:21:53 +0100

    add todo components

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { AutoForm, TextField, AutoFields, SubmitField } from 'uniforms-patternfly';
 
+import { schema } from './schema';
 import { TodoList } from './TodoList';
+import { Loading } from './Loading';
+import { Feedback } from './Feedback';
+import { CREATE_TODO, FIND_TODOS } from './queries';
+
 
 export function Todo() {
+
+  const { loading, error, data } = useQuery(FIND_TODOS, { fetchPolicy: 'network-only'});
+  const [createTodo] = useMutation(CREATE_TODO);
+
+  const handleSubmit = async(model) => {
+    await createTodo({
+      variables: {
+        title: model.title,
+        completed: model.completed,
+      },
+      refetchQueries: [{ query: FIND_TODOS }]
+    });
+    model.title = '';
+  };
+
+  if (loading) return <Loading />;
+
+  if (error) return <Feedback type='error' />;
+
   return (
     <div style={{ width: '40%' }}>
-      <TodoList todos={[]} />
+      <AutoForm schema={schema} onSubmit={handleSubmit} >
+        <TextField name="title" />
+        <SubmitField />
+      </AutoForm>
+      <br/>
+      <TodoList todos={data.findAllTodos} />
     </div>
   );
 };
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Card, CardBody } from '@patternfly/react-core';
+import { useMutation } from '@apollo/react-hooks';
+import { QuickForm, BoolField } from 'uniforms-patternfly';
+import { Card, CardBody, Button } from '@patternfly/react-core';
 
+import { schema } from './schema';
+import { UPDATE_TODO, DELETE_TODO, FIND_TODOS } from './queries';
 
-export function TodoItem() {
+export function TodoItem({ todo }) {
+
+  const [updateTodo] = useMutation(UPDATE_TODO);
+  const [deleteTodo] = useMutation(DELETE_TODO);
+
+  const handleToggle = () => {
+    updateTodo({
+      variables: {
+        id: todo.id,
+        completed: !todo.completed,
+      },
+    });
+  };
+
   return (
     <Card style={{ marginBottom: '0.5em' }}>
       <CardBody>
-        
+        <QuickForm schema={schema} >
+          <BoolField
+            label={todo.title}
+            value={todo.completed}
+            name="completed"
+            appearance="switch"
+            onChange={handleToggle}
+          />
+        </QuickForm>
+        <Button 
+          variant="danger"
+          style={{ float: 'right', marginTop: '0.5em' }}
+          onClick={() => {
+            deleteTodo({
+              variables: { id: todo.id },
+              refetchQueries: [{ query: FIND_TODOS }],
+            })
+          }}
+        >
+          Delete
+        </Button>
       </CardBody>
     </Card>
   );
